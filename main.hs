9912ee9a9b984739386fb4e47e1944ca6e731246
9ziggy9 <davidarogers@protonmail.com>
2024-02-28 00:58:32 -0800

    small amount of progress

--- main.hs
+++ main.hs
@@ -1,9 +1,14 @@
+import Data.List (find)
+import Data.Maybe (catMaybes, fromJust)
+
 type WireId = Int
 type GateId = Int
 type Signal = Bool
 
 data GateType =
-  Not
+  Input
+  | Output
+  | Not
   | Or
   | And
   | Xor
@@ -13,31 +18,49 @@ data GateType =
 data Gate = Gate { gateId :: GateId, gateType :: GateType } deriving (Show, Eq)
 
 data Wire = Wire {
-  wireId :: WireId,
+  wireId   :: WireId,
   fromGate :: GateId,
-  toGate :: GateId,
-  signal :: Signal
+  toGate   :: GateId,
+  signal   :: Signal
 } deriving (Show, Eq)
 
-data Circuit = Circuit { gates :: [Gate], wires :: [Wire] } deriving (Show, Eq)
+-- Representation of directed graph.
+data Circuit = Circuit {
+  gates   :: [Gate],
+  wires   :: [Wire]
+} deriving (Show, Eq)
+
+-- Function to evaluate a gate given its type and input signals
+evalGate :: GateType -> [Signal] -> Signal
+evalGate Not  [a]    = not a
+evalGate And  [a, b] = a && b
+evalGate Or   [a, b]  = a || b
+evalGate Xor  [a, b] = a /= b
+evalGate Nor  [a, b] = not (a || b)
+evalGate Nand [a, b] = not (a && b)
+evalGate _ _         = error "Invalid input"
+
+test_evalGate :: IO ()
+test_evalGate = do
+  print (evalGate Not [True])
+  print (evalGate And [True, False])
+  print (evalGate Or  [True, False])
+  print (evalGate Xor [True, False])
+
+test_circuit :: IO ()
+test_circuit = do
+  let in1 = Gate { gateId = 1, gateType = Input }
+      in2 = Gate { gateId = 2, gateType = Input }
+  let out1 = Gate { gateId = 3, gateType = Output }
+      out2 = Gate { gateId = 4, gateType = Output }
+  print Circuit {
+    gates = [in1, in2, out1, out2],
+    wires = []
+  }
 
 main :: IO ()
 main = do
-  let g1 = Gate { gateId = 1, gateType = And }
-      g2 = Gate { gateId = 2, gateType = Not }
-
-      wire_g1_g2 = Wire {
-        wireId = 1,
-        fromGate = 1,  -- Output from g1 (And)
-        toGate = 2,    -- Input to g2 ( Not )
-        signal = False --  Supply to circuit
-      }
-
-      nandCircuit = Circuit {
-        gates = [g1, g2],
-        wires = [wire_g1_g2]
-      }
-
-  print nandCircuit
+  test_evalGate
+  test_circuit
 
   
